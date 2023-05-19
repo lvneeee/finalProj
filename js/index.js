994f5e5096e1c73108693e53eadcde906b793dc4
Le Viet <levietnee@gmail.com>
2023-05-19 08:17:32 +0700

    sap xong r

--- js/index.js
+++ js/index.js
@@ -36,12 +36,10 @@ $(document).ready(function() {
         if (window.scrollY >= 10) {
             header.addClass('bg_light');
             links.addClass('text_dark');
-            // Lưu trạng thái của .header vào localStorage
             localStorage.setItem('headerState', 'bg_light');
         } else {
             header.removeClass('bg_light');
             links.removeClass('text_dark');
-             // Xóa trạng thái của .header khỏi localStorage
             localStorage.removeItem('headerState');
         }
     });
@@ -66,16 +64,14 @@ $(document).ready(function() {
         }
     });
 
-    //! hàm xử lí menu
-
     // loop vid 
-    var bgVideo = document.getElementById("main_video");
-    bgVideo.addEventListener("ended", function() {
+    const bgVideo = $("#main_video").get(0);
+    bgVideo.onended = function() {
     setTimeout(function() {
         bgVideo.currentTime = 0;
         bgVideo.play();
-    }, 1500);
-    });
+        }, 1500);
+    };
 
     // parallax .section_1
     $(document).on('scroll', function() {
@@ -101,8 +97,139 @@ $(document).ready(function() {
         }, 200);
     }
     
-    animateText('brand_animation', ['MICHT', 'Extreme Generation'], 6);
-    animateText('new_collection_animation', ['LA FEMME Collection', 'Coming soon...'], 6);
+    animateText('brand_animation', ['Extreme Gen-'], 6);
+    animateText('brand_animation_2', ['MICHT'], 6);
+
+    // add to cart
+    let cartItems = [];
+
+    $(".add_to_cart").click(function() {
+        const productId = $(this).data("id");
+        const productName = $(this).data("name");
+        const productPrice = $(this).data("price");
+        const productElement = $(this).closest(".cards");
+        const productImage = productElement.find(".image img").attr("src");
+
+        const product = {
+            id: productId,
+            name: productName,
+            price: productPrice,
+            image: productImage, // Thêm thuộc tính image vào sản phẩm
+            quantity: 1
+        };
+        addToCart(product);
+        updateCart();
+    });
+
+    function addToCart(product) {
+        if (!product || !product.id || !product.name || !product.price || !product.image) {
+            return;
+        }
+
+        let productAlreadyInCart = false;
+        for (let i = 0; i < cartItems.length; i++) {
+            if (cartItems[i].id === product.id) {
+            cartItems[i].quantity++;
+            productAlreadyInCart = true;
+            break;
+            }
+        }
+        if (!productAlreadyInCart) {
+            cartItems.push(product);
+        }
+        updateCart();
+    }
+
+    function removeCartItem(index) {
+        cartItems.splice(index, 1);
+        updateCart();
+    }
+
+    function subtractQuantity(index) {
+        if (cartItems[index].quantity > 1) {
+            cartItems[index].quantity--;
+            updateCart();
+        }
+    }
+
+    function addQuantity(index) {
+        cartItems[index].quantity++;
+        updateCart();
+    }
+
+    function updateCart() {
+        const cart = $("#cart_list");
+        const cartCount = $("#cart_count");
+        const cartTotal = $("#cart_total");
+        const cartActions = $("#cart_actions")
+        const cartNoti = $("#cart_noti")
+
+        cart.empty();
+        let totalQuantity = 0;
+        let totalPrice = 0;
+        
+        for (let i = 0; i < cartItems.length; i++) {
+            const item = cartItems[i];
+            const itemElement = $("<li></li>").addClass("cart-item");
+            const itemImage = $("<img>").attr("src", item.image).addClass("cart-item-image");
+            const itemDetail = $("<div></div>").addClass("item_detail");
+            const itemName = $("<div></div>").addClass("product_name").text(item.name);
+            const itemPrice = $("<div></div>").addClass("product_price").text(item.price);
+            const itemQuantity = $("<div></div>").addClass("quantity_select");
+            const itemQuantityMinusBtn = $("<button>-</button>").on("click", function() {
+                subtractQuantity(i); 
+            });
+            const itemQuantityInput = $("<input>").attr("type", "text").attr("value", item.quantity).prop("disabled", true);
+            const itemQuantityPlusBtn = $("<button>+</button>").on("click", function () {
+                addQuantity(i); 
+            });
+            const itemRemoveBtn = $("<button><ion-icon name='close'></ion-icon></button>").on("click", function () {
+                removeCartItem(i); 
+            });
+            itemQuantity.append(itemQuantityMinusBtn, itemQuantityInput, itemQuantityPlusBtn);
+            itemDetail.append(itemRemoveBtn, itemName, itemPrice, itemQuantity);
+            itemElement.append(itemImage, itemDetail);
+            cart.append(itemElement);
+            totalQuantity += item.quantity;
+            totalPrice += item.quantity * item.price;
+        }
+
+        cartCount.text(totalQuantity);
+        if (totalQuantity > 0) {
+            cartCount.removeClass("hide");
+        } else {
+            cartCount.addClass("hide");
+        }
+
+        cartTotal.text(totalPrice.toFixed(2));
+        if (totalPrice > 0) {
+            cartTotal.removeClass("hide");
+            cartActions.removeClass("hide");
+            cartNoti.addClass("hide");
+            cart.removeClass("hide");
+        } else {
+            cart.addClass("hide")
+            cartTotal.addClass("hide");
+            cartActions.addClass("hide");
+            cartNoti.removeClass("hide").addClass("show");
+        } 
+
+        console.log("cartItems:", cartItems);
+        console.log("totalQuantity:", totalQuantity);
+    }
+
+
+    // hàm xử lý menu
+    $('.dropend').each(function() {
+        $(this).hover(
+            function() {
+                $(this).find('.dropend_menu').toggleClass('show');
+            },
+            function() {
+                $(this).find('.dropend_menu').toggleClass('show');
+            }
+        );
+    });
 
     // splide
     var splide = new Splide('#image_carousel', {
@@ -113,6 +240,13 @@ $(document).ready(function() {
         perPage: 1,
         pagination: false,
         arrows: false,
+        width : '100%',
+		height: '750px',
+        brakepoints: {
+            1200: { width: '1200px', height: '750px', cover: false},
+            800: { width: '800px', height: '600px', cover: true},
+        },
+        // cover: true,
     });
     splide.mount();
 });
